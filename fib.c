710ae281022493d81cc1817bcedf025fcb7018a5
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-09-22 12:57:44 -0700

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
-   return 0;
+unsigned long long fibonacci_iterative(int position) {
+    if (position == 1) {
+        return 0;
+    } else if (position == 2) {
+        return 1;
+    }
+    
+    unsigned long long first = 0;
+    unsigned long long second = 1;
+    unsigned long long next;
+    for (int current = 3; current <= position; current++) {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return second;
+}
+
+unsigned long long int fibonacci_recursive(int position) {
+    if (position == 1) {
+        return 0;
+    } else if (position == 2) {
+        return 1;
+    }
+    return fibonacci_recursive(position - 1) + fibonacci_recursive(position - 2);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <integer> <method (r/i)> <filename>\n", argv[0]);
+        return 1;
+    }
+
+    int command_line_input = atoi(argv[1]);
+    char calculation_method = argv[2][0];
+
+    FILE *input_file = fopen(argv[3], "r");
+    if (!input_file) {
+        printf("Error: could not open file %s\n", argv[3]);
+        return 1;
+    }
+
+    int file_input;
+    fscanf(input_file, "%d", &file_input);
+    fclose(input_file);
+
+    int target_position = command_line_input + file_input;
+
+    unsigned long long int fibonacci_result;
+    if (calculation_method == 'i') {
+        fibonacci_result = fibonacci_iterative(target_position);
+    } else if (calculation_method == 'r') {
+        fibonacci_result = fibonacci_recursive(target_position);
+    } else {
+        printf("Invalid method specified. Use 'i' for iterative or 'r' for recursive.\n");
+        return 1;
+    }
+
+    printf("%llu\n", fibonacci_result);
+
+    return 0;
 }
